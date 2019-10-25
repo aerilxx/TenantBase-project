7689c8bab05c1e0cb84b3cf2afe01533e640d09e
aerilxx <xiebingqingaeril@gmail.com>
2019-10-24 21:06:20 -0700

    change bash file to run my server

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# if you don't have existing db to display on index, instatiate 
-python backend/create_db.py
 
 #install memcached
-sudo apt-get install memcached
-service memcached restart
+#sudo apt-get install memcached
+#service memcached restart
 
 # open memcached port 
-telnet localhost 11211
+#telnet localhost 11211
 
 # run main program
 python backend/main.py
+
+python backend/memcache_like_server.py
